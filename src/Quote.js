bc2fda2c2fc8f201d0e79f4ee0739730ce47ea16
Balazs Kosa <b4l4g3@gmail.com>
2018-12-08 11:15:48 +0100

    Add Buttons Component

--- src/Quote.js
+++ src/Quote.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import './Quote.css'
+
 class Quote extends Component {
      
 
   render() {
-    const quoteQuote = this.props.quoteQuote;
+    const quoteText = this.props.quoteText;
     const quoteAuthor = this.props.quoteAuthor;
       return (
         <div>
-          <p class="quoteText"><i class="fa fa-quote-left"></i><span class="js-quoteText">{quoteQuote}</span></p>
+          <p class="quoteText"><i class="fa fa-quote-left"></i><span class="js-quoteText">{quoteText}</span></p>
           <p class="quoteAuthor js-quoteAuthor">{quoteAuthor}</p>
         </div>
       );
