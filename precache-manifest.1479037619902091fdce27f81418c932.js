c407c5ea20cc1ca5a67ce047ef85c226ed517f88
Balazs Kosa <b4l4g3@gmail.com>
2018-12-09 20:23:12 +0100

    Updates

---
+++ precache-manifest.1479037619902091fdce27f81418c932.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "16517131e3394ecd50f3",
+    "url": "/static/js/main.16517131.chunk.js"
+  },
+  {
+    "revision": "1d714d2257181748a59b",
+    "url": "/static/js/1.1d714d22.chunk.js"
+  },
+  {
+    "revision": "16517131e3394ecd50f3",
+    "url": "/static/css/main.612625c6.chunk.css"
+  },
+  {
+    "revision": "84a8cdbb42b7c5befcded31ad18ce3e3",
+    "url": "/index.html"
+  }
+];
