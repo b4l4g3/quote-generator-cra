995c29b17b3e3fe74a7e60898773b6ba1a9838f8
Balazs Kosa <b4l4g3@gmail.com>
2018-12-11 00:46:36 +0100

    Add Button hover

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Quote from './Quote';
 import Buttons from './Buttons';
 import { rng, tweet, tumblr } from './helper.js';
 import db from './db.json';
-import styled from 'styled-components'
+import styled, {ThemeProvider} from 'styled-components'
 
 const Container = styled.div`
   min-height: 100vh;
@@ -27,21 +27,24 @@ const Main = styled.main`
   border-radius: 3.5px;
 
   @media (min-width: 700px) {
-      flex-grow: 0;
-      max-width: 700px;
+    flex-grow: 0;
+    max-width: 700px;
   }
 
   @media (max-width: 350px) {
       justify-content: flex-start;
   }
 `
+const theme = {
+  main: "mediumseagreen"
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       quotes: [],
-      randomColor: `hsl(${rng(360)}, 70%, 70%)`
+      random: rng(360)
     };
     this.getQuote = this.getQuote.bind(this);
   }
@@ -54,32 +57,32 @@ class App extends Component {
   getQuote() {
     this.setState({
       quotes: db.quotes[rng(101)],
-      randomColor: `hsl(${rng(360)}, 70%, 70%)`
+      random: rng(360)
     })
   }
 
   render() {
     const quotes = this.state.quotes;
     const getQuote = this.getQuote;
-    const color = {
-      backgroundColor: this.state.randomColor
-    }
-    const fAws = {
+    const randomColor = `hsl(${this.state.random}, 70%, 70%)`;
+    const fontAws = {
       paddingRight: '0.5em',
       fontSize: '1.4em',
-      color: `${this.state.randomColor}`
+      color: `${randomColor}`
     }
     const tweetPost = function () {
       tweet(quotes)
     }
     const tumblrPost = () => tumblr(quotes);
       return (
-        <Container bgColor={this.state.randomColor}>
+        <Container bgColor={randomColor}>
           <Main>
             <div>
-              <Quote quoteText={quotes.quote} quoteAuthor={quotes.author} iconStyle={fAws} />
-              <Buttons buttonColor={color} buttonNewQuote={getQuote} tweetFunc={tweetPost} tumblrFunc={tumblrPost} />
-            </div>
+              <Quote quoteText={quotes.quote} quoteAuthor={quotes.author} iconStyle={fontAws} />
+              <ThemeProvider theme={theme}> 
+              <Buttons buttonColor={randomColor} hoverColor={`hsl(${this.state.random}, 75%, 75%)`} buttonNewQuote={getQuote} tweetFunc={tweetPost} tumblrFunc={tumblrPost} />
+              </ThemeProvider>
+              </div>
           </Main>
         </Container>
       );
